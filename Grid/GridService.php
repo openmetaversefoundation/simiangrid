4d9e47fb65939f7bf63f522162a1111e055b4ca6
jimradford <jimradford@7241c5be-0a0a-11df-a92b-075ba6b65aa2>
2010-02-06 10:01:19 +0000

    * Switch GridService to use json encoded results instead of http error codes * GridService now supports application/json encoded request data in addition to application/x-www-form-urlencoded
    git-svn-id: http://openmetaverse.googlecode.com/svn/trunk/Simian@223 7241c5be-0a0a-11df-a92b-075ba6b65aa2

--- Grid/GridService.php
+++ Grid/GridService.php
@@ -32,19 +32,22 @@
  * @license    http://www.debian.org/misc/bsd.license  BSD License (3 Clause)
  * @link       http://openmetaverse.googlecode.com/
  */
- ob_start();
 
-      $fh = fopen("debug.log", 'w');
-         echo '--- $_SERVER';
-         print_r($_SERVER);
-         echo '--- $_GET';
-         print_r($_GET);
-         echo '--- $_POST';
-         print_r($_POST);
-
-         fwrite($fh, ob_get_contents());
-         ob_end_clean();
-      fclose($fh);
+    /*
+     * This section is used for debugging purposes, for production use it should be disabled
+     */
+    ob_start();
+    $fh = fopen("debug.log", 'w');
+    echo '--- $_SERVER';
+    print_r($_SERVER);
+    echo '--- $_GET';
+    print_r($_GET);
+    echo '--- $_POST';
+    print_r($_POST);
+    fwrite($fh, ob_get_contents());
+    ob_end_clean();
+    fclose($fh);
+    /* End of developer debugging */
 
     require_once('lib/Class.Logger.php');
     $L = new Logger('services.ini', "GRIDSERVICE");
@@ -57,22 +60,42 @@
     require_once('lib/Class.MySQL.php');
     require_once('lib/Class.Factory.php');
 
-    if(isset($_POST['type']) && $_POST['type'] != 'default')
+    if($_SERVER['CONTENT_TYPE']=='application/x-www-form-urlencoded')
     {
-        $logger->warning('An Unsupported identity type: '. $_POST['type'] .' was requested by client');
-        header("HTTP/1.0 405 Method Not Allowed");
+        $REQUEST_DATA = $_POST;
+    }
+    else if($_SERVER['CONTENT_TYPE']=='application/json')
+    {
+        $json = json_decode($HTTP_RAW_POST_DATA,true);
+        if(json_last_error()==JSON_ERROR_NONE)
+        {
+            $REQUEST_DATA = $json;
+        } else {
+            $logger->debug("error decoding json data: " . json_last_error());
+            header("X-Powered-By: Simian Grid Services", true);
+            header("Content-Type: application/json", true);
+            echo '{"Success":false, "Message":"Error while decoding json data"}';
+            exit;
+        }
+    }
+    else
+    {
+        $logger->debug("Invalid Content-Type sent: " . $_SERVER['CONTENT_TYPE']);
         header("X-Powered-By: Simian Grid Services", true);
+        header("Content-Type: application/json", true);
+        echo '{"Success":false, "Message":"Content-type not set or invalid"}';
         exit;
     }
 
-    if(isset($_POST['RequestMethod']) && $_POST['RequestMethod'] != '')
+    $command = trim($REQUEST_DATA['RequestMethod']);
+
+    if(empty($command))
     {
-        $command = trim($_POST['RequestMethod']);
-    } else {
         $logger->warning('An Unsupported command, or empty command or parameter was requested by client');
-        $logger->debug(print_r($_POST, true));
-        header("HTTP/1.0 405 Method Not Allowed");
+        $logger->debug(print_r($REQUEST_DATA, true));
         header("X-Powered-By: Simian Grid Services", true);
+        header("Content-Type: application/json", true);
+        echo '{"Success":false, "Message":"Unsupported or missing RequestMethod"}';
         exit;
     }
 
@@ -84,8 +107,9 @@
         }
         catch(Exception $ex)
         {
-            header("HTTP/1.1 400 Bad Request");
             header("X-Powered-By: Simian Grid Services", true);
+            header("Content-Type: application/json", true);
+            echo '{"Success":false, "Message":"Exception while trying to create instance of '.$command.'"}';
             $logger->debug("Exception in Factory: " . print_r($ex,true));
             exit;
         }
@@ -93,8 +117,8 @@
     else
     {
         $logger->warning('An Unsupported request method: '. $_SERVER['REQUEST_METHOD'] .' was requested by client');
-        header("HTTP/1.0 405 Method Not Allowed");
         header("X-Powered-By: Simian Grid Services", true);
+        echo '{"Success":false, "Message":"Unsupported http request method '.$_SERVER['REQUEST_METHOD'].'"}';
         exit;
     }
 
@@ -108,10 +132,10 @@
     }
     catch (Exception $ex)
     {
-        header("HTTP/1.1 500 Internal Server Error");
-        header("X-Powered-By: Simian Grid Services", true);
         $logger->crit(sprintf("Database Exception: %d %s", mysqli_connect_errno(), mysqli_connect_error()));
         $logger->debug(sprintf("Database Exception: %s", print_r($ex,true)));
+        header("X-Powered-By: Simian Grid Services", true);
+        echo '{"Success":false, "Message":"An error ocurred while attempting to connect or communicate with the database"}';
         exit();
     }
 
@@ -120,19 +144,20 @@
     {
         try 
         {
-            $Action->Execute($db, $_POST, $logger);
+            $Action->Execute($db, $REQUEST_DATA, $logger);
         }
         catch (Exception $ex)
         {
-            header("HTTP/1.1 500 Internal Server Error");
             header("X-Powered-By: Simian Grid Services", true);
             $logger->debug(sprintf("Call Execute Exception: %s", print_r($ex,true)));
+            echo '{"Success":false, "Message":"An exception ocurred trying to Execute class"}';
             exit();
         }
     }
     else
     {
-        header("HTTP/1.1 405 Method Not Allowed");
         header("X-Powered-By: Simian Grid Services", true);
+        echo '{"Success":false, "Message":"Unable to load requested action"}';
         exit;
     }
+?>
