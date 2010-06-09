51b1baf798445dc3f0ea9db159bdbc3e24535a6b
coyle@knifejaw.com <coyle@knifejaw.com@7241c5be-0a0a-11df-a92b-075ba6b65aa2>
2010-06-09 01:08:05 +0000

    Explicitly default user_data and user_agent columns in sgf_sessions to null.
    git-svn-id: http://openmetaverse.googlecode.com/svn/trunk/Simian@417 7241c5be-0a0a-11df-a92b-075ba6b65aa2

--- GridFrontend/schema.sql
+++ GridFrontend/schema.sql
@@ -15,9 +15,9 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE IF NOT EXISTS `sgf_sessions` (
   `session_id` varchar(40) collate utf8_bin NOT NULL default '0',
   `ip_address` varchar(16) collate utf8_bin NOT NULL default '0',
-  `user_agent` varchar(150) collate utf8_bin NOT NULL,
+  `user_agent` varchar(150) collate utf8_bin NOT NULL default NULL,
   `last_activity` int(10) unsigned NOT NULL default '0',
-  `user_data` text collate utf8_bin NOT NULL,
+  `user_data` text collate utf8_bin NOT NULL default NULL,
   PRIMARY KEY  (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
