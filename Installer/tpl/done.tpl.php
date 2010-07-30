a57c4f59c91924ddf0fa587a63461e17b2f06b2c
jonafree <jonafree@7241c5be-0a0a-11df-a92b-075ba6b65aa2>
2010-07-30 15:49:03 +0000

    * forgot an installer template
    git-svn-id: http://openmetaverse.googlecode.com/svn/trunk/Simian@473 7241c5be-0a0a-11df-a92b-075ba6b65aa2

---
+++ Installer/tpl/done.tpl.php
@@ -0,0 +1,5 @@
+<?php if ( ! defined('BASEPATH') or !defined('SIMIAN_INSTALLER') ) exit('No direct script access allowed'); ?>
+<?php require 'header.tpl.php'; ?>
+<h2>Complete</h2>
+
+<?php require 'footer.tpl.php'; ?>
