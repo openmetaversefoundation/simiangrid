92be616a183983107d62e96d60c69fa472634567
Hiroaki Protagonist <james.stallings@gmail.com>
2010-09-25 18:18:33 -0500

    Fix interesting SQL clause in schema.sql: NOT NULL DEFAULT ''

--- GridFrontend/schema.sql
+++ GridFrontend/schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS `sgf_sessions` (
   `session_id` varchar(40) collate utf8_bin NOT NULL default '0',
   `ip_address` varchar(16) collate utf8_bin NOT NULL default '0',
-  `user_agent` varchar(150) collate utf8_bin NOT NULL default '',
+  `user_agent` varchar(150) collate utf8_bin NOT NULL,
   `last_activity` int(10) unsigned NOT NULL default '0',
-  `user_data` text collate utf8_bin NOT NULL default '',
+  `user_data` text collate utf8_bin NOT NULL,
   PRIMARY KEY  (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
