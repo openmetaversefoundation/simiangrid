aff641b36f04711c8cc56686804bc142da11afc4
Jonathan Freedman <jonafree@gmail.com>
2010-09-27 00:46:07 -0400

    Revert "Merge branch 'master' of http://github.com/JamesStallings/simiangrid"
    This reverts commit 077e9d95b6c6b14e4e6f03c33a63a586db8a3266, reversing
    changes made to 3030af05d0fa490716e3f130556c6b90360bc598.

---
+++ GridFrontend/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS `sgf_sessions` (
+  `session_id` varchar(40) collate utf8_bin NOT NULL default '0',
+  `ip_address` varchar(16) collate utf8_bin NOT NULL default '0',
+  `user_agent` varchar(150) collate utf8_bin NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL default '0',
+  `user_data` text collate utf8_bin NOT NULL,
+  PRIMARY KEY  (`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `sgf_user_session` (
+	`session_id` VARCHAR(36) NOT NULL,
+	`user_id` VARCHAR(36) NOT NULL,
+	PRIMARY KEY ( `session_id` )
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `sgf_user_settings` (
+	`user_id` VARCHAR(36) NOT NULL,
+	`style` VARCHAR(16) NOT NULL,
+	PRIMARY KEY ( `user_id` )
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `sgf_user_validation` (
+	`user_id` VARCHAR(36) NOT NULL,
+	`validation_code` VARCHAR(36) NOT NULL,
+	`validation_type` VARCHAR(16) NOT NULL,
+	PRIMARY KEY ( `user_id` )
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
