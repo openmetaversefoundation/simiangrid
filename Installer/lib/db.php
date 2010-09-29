e930579ea590f0767ade2ecbaeb816e99d658f53
Hiroaki Protagonist <james.stallings@gmail.com>
2010-09-28 23:49:35 -0500

    general insanity

--- Installer/lib/db.php
+++ Installer/lib/db.php
@@ -236,7 +236,7 @@
     	    $todo = $row[0] + 1;  
 	}
 
-	dbMigrate($db, $todo, $_SESSION['db_config']['db']);
+	dbMigrate($db, $todo,configGetDBValue('db_name') );
     }
 
     function dbMigrate($db, $todo, $store) {
