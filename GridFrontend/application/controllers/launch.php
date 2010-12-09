f6f9462adfb79276173ab9b8351cc61f4785e19b
John Hurliman <john.hurliman@intel.com>
2010-12-08 18:04:42 -0800

    * Added add_capability() to the SimianGrid.php library * Added authenticated launch document support. It will create a 10-minute login capability for the logged in user and return a login capability in the launch doc

--- GridFrontend/application/controllers/launch.php
+++ GridFrontend/application/controllers/launch.php
@@ -9,15 +9,42 @@ class Launch extends Controller {
     
     function index()
     {
-        $data = array();
+        log_message('debug', "Building a launch document");
         
-        // TODO: Support capability generation and name retrieval for logged in sessions
+        $data = array('name' => '', 'region' => '', 'type' => '', 'login_url' => '');
+        $capID = null;
+        
+        if ($this->sg_auth->is_logged_in()) {
+            $user_id = $this->sg_auth->get_uuid();
+            log_message('debug', "Launch document is for logged in user $user_id");
+            
+            $user = $this->simiangrid->get_user($user_id);
+            if ( ! empty($user))
+            {
+                log_message('debug', "$user_id is " . $user['Name']);
+                $data['name'] = $user['Name'];
+            } else {
+                log_message('debug', "Failed to resolve $user_id to an account name");
+            }
+            
+            $timeout_seconds = 10 * 60; // Expires in 10 minutes
+            $capID = $this->simiangrid->add_capability($user_id, 'login', time() + $timeout_seconds);
+        } else {
+            log_message('debug', "Launch document is for unauthenticated user");
+        }
+        
+        if ( ! empty($capID)) {
+            log_message('debug', "Generated capability $capID for $user_id");
+            
+            $data['login_url'] = $this->config->item('login_service') . "?cap=$capID";
+            $data['type'] = 'capability';
+        } else {
+            $data['login_url'] = $this->config->item('login_service');
+        }
+        
         // TODO: Allow region to be passed in as a parameter (sanitize!)
-        $data['login_url'] = $this->config->item('login_service');
-        //$data['name'] = '';
         //$data['region'] = '';
         
-        header("Content-Type: application/calm+json");
         $this->parser->parse('launch', $data);
     }
 }
--- GridFrontend/application/views/launch.php
+++ GridFrontend/application/views/launch.php
@@ -2,23 +2,19 @@
 
 $launch_doc = NULL;
 
-if (isset($name))
-{
-    if (isset($region))
-    {
-        $launch_doc = "{ \"authenticator\": { \"type\": \"capability\" }, \"identifier\": " +
-                     "{ \"type\": \"agent\", \"name\": \"$name\" }, \"loginurl\": \"$login_url\", " +
-                     "\"region\": \"$region\" }";
-    }
-    else
-    {
-        $launch_doc = "{ \"authenticator\": { \"type\": \"capability\" }, \"identifier\": " +
-                     "{ \"type\": \"agent\", \"name\": \"$name\" }, \"loginurl\": \"$login_url\" }";
-    }
-}
-else
-{
-    $launch_doc = "{ \"loginurl\": \"$login_url\" }";
+if ($type === 'capability') {
+    $launch_doc = "{ \"authenticator\": { \"type\": \"capability\" }, \"identifier\": { \"type\": \"agent\", \"name\": \"$name\" }, \"loginurl\": \"$login_url\", \"region\": \"$region\" }";
+} else {
+    $launch_doc = "{ \"loginurl\": \"$login_url\", \"region\": \"$region\" }";
 }
 
-echo($launch_doc);
+// Mark this document as no-cache
+header("Expires: Mon, 20 Dec 1998 01:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+
+// Set the launch document MIME type
+header('Content-type: application/calm+json');
+
+echo $launch_doc;
