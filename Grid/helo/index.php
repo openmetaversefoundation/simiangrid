fe5b7818186c1c3d3456498a782442361c9f0daf
Mic Bowman <cmickeyb@gmail.com>
2012-01-02 17:23:39 -0800

    add logging to helo handler

--- Grid/helo/index.php
+++ Grid/helo/index.php
@@ -1,4 +1,18 @@
 <?php
+$RootPath = "/var/www/Grid";
+
+// This attempts to find the path to the Grid directory
+if (preg_match('/\/.*\/Grid\//',__FILE__,$matches))
+    $RootPath = $matches[0];
+
+define('BASEPATH', $RootPath);
+
+require_once(BASEPATH . 'common/Config.php');
+require_once(BASEPATH . 'common/Errors.php');
+require_once(BASEPATH . 'common/Log.php');
+
+log_message('debug','hypergrid helo request from ' . $_SERVER['REMOTE_ADDR']);
 header("X-Handlers-Provided: opensim-simian");
+
 exit();
 ?>
